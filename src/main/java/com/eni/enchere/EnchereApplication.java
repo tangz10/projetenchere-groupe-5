212f7c0bb9d6b1c76bfd991e230fa809bcd0b6c1
jdegennes2024 <johann.degennes2024@campus-eni.fr>
2025-04-14 14:52:19 +0200

    feat: Ajout projet Java Spring Boot

---
+++ src/main/java/com/eni/enchere/EnchereApplication.java
@@ -0,0 +1,13 @@
+package com.eni.enchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnchereApplication.class, args);
+	}
+
+}
