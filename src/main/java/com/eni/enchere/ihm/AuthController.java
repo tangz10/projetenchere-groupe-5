feeec4407d4febd1a0f23b7421d58d187727b77b
Hugo <hugo.cirette@gmail.com>
2025-04-15 16:40:15 +0200

    login + register + profile_read + BDD

---
+++ src/main/java/com/eni/enchere/ihm/AuthController.java
@@ -0,0 +1,68 @@
+package com.eni.enchere.ihm;
+
+import com.eni.enchere.bo.Utilisateur;
+import com.eni.enchere.dao.Utilisateur.UtilisateurDAOImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Controller
+public class AuthController {
+
+    private final UtilisateurDAOImpl utilisateurDAO;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UtilisateurDAOImpl utilisateurDAO, PasswordEncoder passwordEncoder) {
+        this.utilisateurDAO = utilisateurDAO;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/login")
+    public String viewLogin() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String viewRegister() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(Utilisateur utilisateur, Model model) {/*
+        // vérifier que l'utilisateur n'existe pas déjà
+        if (utilisateurDAO.selectByPseudo(utilisateur.getPseudo())) {
+            model.addAttribute("error", "Ce nom d'utilisateur existe déjà.");
+            return "register";
+        }*/
+
+        utilisateur.setMotDePasse(passwordEncoder.encode(utilisateur.getMotDePasse()));
+        utilisateur.setCredit(0);
+        utilisateur.setAdmin(false);
+        utilisateurDAO.insert(utilisateur);
+
+        return "redirect:/login?registered";
+    }/*
+
+    @GetMapping("/profile/edit")
+    public String profileEdit(Model model) {
+        model.addAttribute("message", "Editez votre profile");
+        return "profile_edit"; // Retourne "login.html" depuis /templates
+    }*/
+
+    @GetMapping("/profile")
+    public String profileRead(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        Utilisateur utilisateurConnecte = utilisateurDAO.selectByPseudo(auth.getName());
+
+        model.addAttribute("utilisateur", utilisateurConnecte);
+
+        return "profile_read";
+    }
+
+}
