33cdb2f52e1589fe06182b0d14f32b51bb0e6aab
Porchie86 <lucas.porcheron86@gmail.com>
2025-04-15 16:35:36 +0200

    Commit Categorie

--- src/main/java/com/eni/enchere/ihm/HomeController.java
+++ src/main/java/com/eni/enchere/ihm/HomeController.java
@@ -1,14 +1,25 @@
 package com.eni.enchere.ihm;
 
+import com.eni.enchere.bo.Categorie;
+import com.eni.enchere.services.CategorieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CategorieService categorieService;
+
+
     @GetMapping("/")
     public String index(Model model) {
+        List<Categorie> categories = categorieService.getAllCategories();
+        model.addAttribute("categories", categories);
         model.addAttribute("message", "Bienvenue sur mon site Spring Boot + Bootstrap !");
         return "index"; // Retourne "index.html" depuis /templates
     }
