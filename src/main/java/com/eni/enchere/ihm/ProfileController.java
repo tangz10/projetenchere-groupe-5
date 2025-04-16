6050d24ba2660e3b9dc7ddba4829b802ff61c717
Hugo <hugo.cirette@gmail.com>
2025-04-16 10:52:36 +0200

    fix bug

--- src/main/java/com/eni/enchere/ihm/ProfileController.java
+++ src/main/java/com/eni/enchere/ihm/ProfileController.java
@@ -4,10 +4,12 @@ import com.eni.enchere.bo.Utilisateur;
 import com.eni.enchere.services.UtilisateurService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class ProfileController {
 
     private final UtilisateurService utilisateurService;
