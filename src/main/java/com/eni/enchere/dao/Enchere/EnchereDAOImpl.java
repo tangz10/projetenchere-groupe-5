38dbe65fbd9d5ca87e891bbe1a8e3c0ba1f6ba52
Porchie86 <lucas.porcheron86@gmail.com>
2025-04-17 10:19:48 +0200

    Commit page encheres fini

--- src/main/java/com/eni/enchere/dao/Enchere/EnchereDAOImpl.java
+++ src/main/java/com/eni/enchere/dao/Enchere/EnchereDAOImpl.java
@@ -3,6 +3,8 @@ package com.eni.enchere.dao.Enchere;
 import com.eni.enchere.bo.ArticleVendu;
 import com.eni.enchere.bo.Enchere;
 import com.eni.enchere.bo.Utilisateur;
+import com.eni.enchere.dao.ArticleVendu.ArticleVenduDAO;
+import com.eni.enchere.dao.Utilisateur.UtilisateurDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,6 +15,12 @@ import java.util.List;
 
 @Repository
 public class EnchereDAOImpl implements EnchereDAO {
+
+    @Autowired
+    private UtilisateurDAO utilisateurDAO;
+
+    @Autowired
+    private ArticleVenduDAO articleVenduDAO;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -36,7 +44,7 @@ public class EnchereDAOImpl implements EnchereDAO {
 
     @Override
     public void insert(Enchere enchere) {
-        String sql = "INSERT INTO Encheres (no_utilisateur, no_article, date_enchere, montant_enchere) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO encheres (no_utilisateur, no_article, date_enchere, montant_enchere) VALUES (?, ?, ?, ?)";
         jdbcTemplate.update(sql,
                 enchere.getNoUtilisateur().getNoUtilisateur(),
                 enchere.getNoArticle().getNoArticle(),
@@ -46,13 +54,72 @@ public class EnchereDAOImpl implements EnchereDAO {
 
     @Override
     public List<Enchere> selectByArticle(long noArticle) {
-        String sql = "SELECT * FROM Encheres WHERE no_article = ?";
+        String sql = "SELECT * FROM encheres WHERE no_article = ?";
         return jdbcTemplate.query(sql, enchereRowMapper, noArticle);
     }
 
     @Override
     public List<Enchere> selectByUtilisateur(long noUtilisateur) {
-        String sql = "SELECT * FROM Encheres WHERE no_utilisateur = ?";
+        String sql = "SELECT * FROM encheres WHERE no_utilisateur = ?";
         return jdbcTemplate.query(sql, enchereRowMapper, noUtilisateur);
     }
+
+    @Override
+    public Enchere findBestOfferByArticleId(long noArticle) {
+        String sql = """
+        SELECT * FROM encheres 
+        WHERE no_article = ? 
+        ORDER BY montant_enchere DESC 
+        LIMIT 1
+    """;
+
+        List<Enchere> result = jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Enchere e = new Enchere();
+
+            // récupérer l'utilisateur complet
+            int idUtilisateur = rs.getInt("no_utilisateur");
+            Utilisateur utilisateur = utilisateurDAO.selectById(idUtilisateur);
+            e.setNoUtilisateur(utilisateur);
+
+            // récupérer l'article complet
+            int idArticle = rs.getInt("no_article");
+            ArticleVendu article = articleVenduDAO.selectById(idArticle);
+            e.setNoArticle(article);
+
+            e.setDateEnchere(rs.getDate("date_enchere").toLocalDate());
+            e.setMontantEnchere(rs.getInt("montant_enchere"));
+
+            return e;
+        }, noArticle);
+
+        return result.isEmpty() ? null : result.get(0);
+    }
+
+    @Override
+    public List<Enchere> selectByUtilisateurEtArticle(long noArticle, long noUtilisateur) {
+        String sql = """
+        SELECT * FROM encheres 
+        WHERE no_article = ? AND no_utilisateur = ?        
+    """;
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Enchere e = new Enchere();
+
+            // récupérer l'utilisateur complet
+            int idUtilisateur = rs.getInt("no_utilisateur");
+            Utilisateur utilisateur = utilisateurDAO.selectById(idUtilisateur);
+            e.setNoUtilisateur(utilisateur);
+
+            // récupérer l'article complet
+            int idArticle = rs.getInt("no_article");
+            ArticleVendu article = articleVenduDAO.selectById(idArticle);
+            e.setNoArticle(article);
+
+            e.setDateEnchere(rs.getDate("date_enchere").toLocalDate());
+            e.setMontantEnchere(rs.getInt("montant_enchere"));
+
+            return e;
+        }, noArticle,noUtilisateur);
+    }
+
 }
