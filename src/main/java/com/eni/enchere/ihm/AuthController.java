70221401bb6440022d6c7db694772981c607b01d
Hugo <hugo.cirette@gmail.com>
2025-04-17 09:45:50 +0200

    en fait les imports étaient utiles

--- src/main/java/com/eni/enchere/ihm/AuthController.java
+++ src/main/java/com/eni/enchere/ihm/AuthController.java
@@ -3,11 +3,14 @@ package com.eni.enchere.ihm;
 import com.eni.enchere.bo.Utilisateur;
 import com.eni.enchere.services.CustomUserDetailsService;
 import com.eni.enchere.services.UtilisateurService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Controller
 public class AuthController {
